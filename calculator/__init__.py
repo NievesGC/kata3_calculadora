45b64d857202bea9bc717e3b5f956c966bfa26f4
Nieves <niieves99@gmail.com>
2023-06-08 23:21:28 +0200

    calculadora primera operacion completa

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,35 +1,49 @@
 import tkinter as tk
-from calculator.controls import Display,CalcButton
-
+from calculator.controls import Display,CalcButton, KeyBoard
+from cromannumbers import RomanNumber
 WIDTH = 272
 HEIGHT = 300
+
+operaciones ={
+     "+": lambda a,b:a+b,
+     "-": lambda a, b: a-b,
+     "x": lambda a, b: a*b,
+     "÷": lambda a, b: a//b
+}
 
 class Calculator(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Calculadora") #ponemos el titulo de la ventana
         self.geometry(f"{WIDTH}x{HEIGHT}") #le decimos el tamaño de la ventana con las constantes que hemos creado arriba
+        self.op1 =  None
+        self.op2 = None 
+        self.operation = None
+        self.value=""
 
         self.display = Display(self) #llamamos a display (que es el contenedor frame donde dentro esta la label)
         self.display.pack() #dibujamos display centrado y arriba porque la geometria pack viene asi predefinida 
-        self.display2 = Display(self) 
-        self.display2.pack(side =tk.BOTTOM) 
-        self.display.typing("ARRIBA") #llamamos e introdicimos el texto que la class dispplay 
-        self.display2.typing("ABAJO")
+        self.display.typing("0")
+        KeyBoard(self,self.clic).pack()
         
-        CalcButton(self,self.clic1,"1").pack() 
-        CalcButton(self,text="2",tiny_wire=self.clic2).pack()
-        CalcButton(self,text="3",tiny_wire=self.clic3).pack()
-        CalcButton(self,text="4",tiny_wire=self.clic4).pack()
 
-    def clic1(self):
-        self.display.typing("1")
+    def clic(self,tecla):
+            if tecla == "AC":
+                self.value = ""
+                self.op1 = self.op2 = self.operation = None
+            elif tecla in(operaciones):
+                self.op1= RomanNumber(self.value)
+                self.operation = tecla
+            elif tecla == "=":
+                 self.op2= RomanNumber(self.value)
+                 resultado = operaciones[self.operation](self.op1,self.op2)
+                 self.value = resultado.simbolo
+            else:
+                if self.operation is not None and self.op2 is None:
+                     self.value = ""
+                     self.op2 = ""
+                self.value+= tecla
+            self.display.typing(self.value)
 
-    def clic2(self):
-        self.display2.typing("2")
-
-    def clic3(self):
-        self.display.typing("3")
-
-    def clic4(self):
-        self.display2.typing("4")
+   
+         
